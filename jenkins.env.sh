cf3bd277dd357aa115001461bb63527ecf600b26
Mikael Lindahl <mickelindahl@gmail.com>
2017-01-27 14:10:00 +0100

    Update jenkins.env.sh

--- jenkins.env.sh
+++ jenkins.env.sh
@@ -9,9 +9,6 @@ TEST_NAME=$PROD_NAME"_test"
 export TEST_NAME
 
 # Grep git askpass token from docker-compose.yml and make available
-GIT_TOKEN_LINE=$(grep -i 'GIT_PERSONAL_ACCESS_TOKEN' $PROD_FOLDER/$PROD_NAME/docker-compose.yml)
-GIT_TOKEN=${GIT_TOKEN_LINE##*:} # Get token
-GIT_TOKEN="$(echo -e "${GIT_TOKEN}" | tr -d '[:space:]')"
-
-# Add credentials to askpass file
-echo "#! /bin/bash" > $GIT_ASKPASS && echo "echo "$GIT_TOKEN >> $GIT_ASKPASS
+GIT_PERSONAL_ACCESS_TOKEN_LINE=$(grep -i 'GIT_PERSONAL_ACCESS_TOKEN' $PROD_FOLDER/$PROD_NAME/docker-compose.yml)
+GIT_PERSONAL_ACCESS_TOKEN=${GIT_PERSONAL_ACCESS_TOKEN_LINE##*:} # Get token
+GIT_PERSONAL_ACCESS_TOKEN="$(echo -e "${GIT_PERSONAL_ACCESS_TOKEN}" | tr -d '[:space:]')"
