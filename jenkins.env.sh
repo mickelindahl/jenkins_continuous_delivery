de3e9c56664fd1ac91dc813e2e972723415578fd
Mikael Lindahl <mickelindahl@gmail.com>
2017-01-27 14:06:38 +0100

    askpass changes

--- jenkins.env.sh
+++ jenkins.env.sh
@@ -3,13 +3,15 @@ export GIT_PROJECT=greendrive
 export TEST_FOLDER=/opt/apps/test
 export PROD_FOLDER=/opt/apps
 export PROD_NAME=greendrive
+export GIT_ASKPASS=/opt/apps/git_askpass.sh
 
 TEST_NAME=$PROD_NAME"_test"
 export TEST_NAME
 
 # Grep git askpass token from docker-compose.yml and make available
-GIT_ASKPASS_LINE=$(grep -i 'GIT_ASKPASS' $PROD_FOLDER/$PROD_NAME/docker-compose.yml)
-GIT_ASKPASS=${GIT_ASKPASS_LINE##*:} # Get token
-GIT_ASKPASS="$(echo -e "${GIT_ASKPASS}" | tr -d '[:space:]')"
+GIT_TOKEN_LINE=$(grep -i 'GIT_PERSONAL_ACCESS_TOKEN' $PROD_FOLDER/$PROD_NAME/docker-compose.yml)
+GIT_TOKEN=${GIT_TOKEN_LINE##*:} # Get token
+GIT_TOKEN="$(echo -e "${GIT_TOKEN}" | tr -d '[:space:]')"
 
-export GIT_ASKPASS
+# Add credentials to askpass file
+echo "#! /bin/bash" > $GIT_ASKPASS && echo "echo "$GIT_TOKEN >> $GIT_ASKPASS
